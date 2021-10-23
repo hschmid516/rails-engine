4007edc5e8d4977d2ebe8646af6eef7655ff3dc6
Henry Schmid <henry.schmid1@gmail.com>
2021-10-23 12:55:35 -0600

    Add functionality for page and per_page optional params

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    merchants = Merchant.all
+    per_page = params.fetch(:per_page, 20)
+    page =  per_page * (params.fetch(:page, 1).to_i - 1)
+    merchants = Merchant.limit(per_page).offset(page)
     render json: MerchantSerializer.new(merchants)
   end
 end
