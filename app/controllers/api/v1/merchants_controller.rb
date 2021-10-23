cf24a05ccbbc6fe4356deb60cf34c7ffe64f6d92
Henry Schmid <henry.schmid1@gmail.com>
2021-10-23 13:02:44 -0600

    Add sad path for page < 1

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
     per_page = params.fetch(:per_page, 20)
-    page =  per_page * (params.fetch(:page, 1).to_i - 1)
+    if params[:page].to_i > 0
+      page =  per_page * (params.fetch(:page, 1).to_i - 1)
+    else
+      page = 0
+    end
     merchants = Merchant.limit(per_page).offset(page)
     render json: MerchantSerializer.new(merchants)
   end
