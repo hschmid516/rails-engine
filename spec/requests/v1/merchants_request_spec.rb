6ee80491a774b9fff53051de71a8750cdf08e2e1
Henry Schmid <henry.schmid1@gmail.com>
2021-10-23 14:12:55 -0600

    Add tests for merchants sad paths

--- spec/requests/v1/merchants_request_spec.rb
+++ spec/requests/v1/merchants_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'merchants API' do
   it 'gets  merchants' do
-    create_list(:merchant, 25)
+    create_list(:merchant, 3)
 
     get '/api/v1/merchants'
 
@@ -30,14 +30,13 @@ describe 'merchants API' do
     merchants = JSON.parse(response.body, symbolize_names: true)[:data]
 
     expect(merchants.count).to eq(20)
-
     expect(merchants.first[:id].to_i).to eq(Merchant.first.id)
   end
 
   it 'can take per_page and page params to limit and offset' do
     create_list(:merchant, 50)
 
-    get '/api/v1/merchants', params: { per_page: 10, page: 2}
+    get '/api/v1/merchants', params: { per_page: 10, page: 2 }
 
     expect(response).to be_successful
 
@@ -45,5 +44,38 @@ describe 'merchants API' do
 
     expect(merchants.count).to eq(10)
     expect(merchants.first[:id].to_i).to eq(Merchant.offset(10).first.id)
+  end
+
+  it 'fetches 1 page if page param is < 1 or non-integer' do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants', params: { page: 0 }
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(merchants.count).to eq(3)
+
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(String)
+    end
+
+    get '/api/v1/merchants', params: { page: -5 }
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(merchants.count).to eq(3)
+
+    get '/api/v1/merchants', params: { page: 'page 8' }
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(merchants.count).to eq(3)
   end
 end
