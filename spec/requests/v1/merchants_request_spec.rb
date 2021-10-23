7e04a2a711b5f99d65b3b059dcabb422f948666c
Henry Schmid <henry.schmid1@gmail.com>
2021-10-23 11:42:45 -0600

    Add attributes to merchant test

--- spec/requests/v1/merchants_request_spec.rb
+++ spec/requests/v1/merchants_request_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
 describe 'merchants API' do
-  it 'gets all merchants 20 at a time' do
+  it 'gets  merchants' do
     create_list(:merchant, 25)
 
     get '/api/v1/merchants'
 
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(merchants).to be_an(Array)
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(String)
+
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
+    end
   end
+
 end
