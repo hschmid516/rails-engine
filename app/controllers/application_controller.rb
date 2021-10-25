0f4033d3978d847a469a9d049a3867ecf9ad9058
Henry Schmid <henry.schmid1@gmail.com>
2021-10-24 21:01:13 -0600

    Update error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   def no_merchant_error
     render json: {
         message: "your query could not be completed",
-        errors: ["no merchant found with id: #{params[:id]}"],
+        errors: ["no merchant found with id: #{params[:merchant_id]}"],
       }, status: 404
   end
 end
