29ac8201c94a7251b033d2e922dc42a708d659ae
Henry Schmid <henry.schmid1@gmail.com>
2021-10-24 18:43:07 -0600

    Add test for getting one item and sad path

--- spec/requests/v1/items_request_spec.rb
+++ spec/requests/v1/items_request_spec.rb
@@ -83,6 +83,38 @@ describe 'items API' do
 
     expect(items.count).to eq(20)
   end
+
+  it 'can get one item' do
+    get "/api/v1/items/#{Item.first.id}"
+
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(item).to be_a(Hash)
+    expect(item[:data]).to be_a(Hash)
+    expect(item[:data]).to have_key(:id)
+    expect(item[:data][:id]).to be_an(String)
+    expect(item[:data][:attributes]).to have_key(:name)
+    expect(item[:data][:attributes][:name]).to be_a(String)
+    expect(item[:data][:attributes]).to have_key(:description)
+    expect(item[:data][:attributes][:description]).to be_a(String)
+    expect(item[:data][:attributes]).to have_key(:unit_price)
+    expect(item[:data][:attributes][:unit_price]).to be_a(Float)
+    expect(item[:data][:attributes]).to have_key(:merchant_id)
+    expect(item[:data][:attributes][:merchant_id]).to be_an(Integer)
+    expect(item[:data][:attributes]).to_not have_key(:created_at)
+    expect(item[:data][:attributes]).to_not have_key(:updated_at)
+    expect(item[:data][:attributes]).to_not have_key(:created_at)
+    expect(item[:data][:attributes]).to_not have_key(:updated_at)
+  end
+
+  it 'has 404 error if bad id' do
+    get "/api/v1/items/10000"
+
+    expect(response).to_not be_successful
+    expect(response).to have_http_status(404)
+  end
 end
 
 describe '1 or 0 items' do
