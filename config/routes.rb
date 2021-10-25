373d496901fa558da034e942d53db82f4ee31e27
Henry Schmid <henry.schmid1@gmail.com>
2021-10-24 18:47:20 -0600

    Add to items resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :items, only: :index
+      resources :items, only: [:index, :show] do
+        get 'merchant', to: 'items_merchant#show'
+      end
       resources :merchants, only: [:index, :show] do
         get 'items', to: 'merchant_items#index'
       end
