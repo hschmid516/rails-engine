acaf415659876dc1aa745a453da9c164cea8a5bd
Henry Schmid <henry.schmid1@gmail.com>
2021-10-26 15:50:03 -0600

    Add #order_by_revenue test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -9,4 +9,26 @@ RSpec.describe Merchant, type: :model do
 
     expect(Merchant.find_by_name('LaN')).to eq(merch1)
   end
+
+  it '#order_by_revenue' do
+    @merch1 = create(:merchant)
+    item1 = create(:item, merchant: @merch1)
+    cust1 = create(:customer)
+    inv1 = @merch1.invoices.create(customer: cust1)
+    inv1.invoice_items.create(item: item1, quantity: 10, unit_price: 100)
+
+    @merch2 = create(:merchant)
+    item2 = create(:item, merchant: @merch2)
+    cust2 = create(:customer)
+    inv2 = @merch2.invoices.create(customer: cust2)
+    inv2.invoice_items.create(item: item2, quantity: 10, unit_price: 200)
+
+    @merch3 = create(:merchant)
+    item3 = create(:item, merchant: @merch3)
+    cust3 = create(:customer)
+    inv3 = @merch3.invoices.create(customer: cust3)
+    inv3.invoice_items.create(item: item3, quantity: 10, unit_price: 300)
+
+    expect(Merchant.order_by_revenue(2)).to eq([@merch3, @merch2])
+  end
 end
