f0a37ef1e7bc06aa7aa40572b0bf279a88c0b71f
Henry Schmid <henry.schmid1@gmail.com>
2021-10-25 20:33:21 -0600

    Add model test for find_by_name

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
   it { should have_many(:items) }
+
+  it '#find_by_name' do
+    merch1 = create(:merchant, name: 'Icelantic')
+    merch2 = create(:merchant, name: 'Land Rover')
+
+    expect(Merchant.find_by_name('LaN')).to eq(merch1)
+  end
 end
