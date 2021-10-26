c31dfc4aaf919f04eac2773564970e631862c8c8
Henry Schmid <henry.schmid1@gmail.com>
2021-10-26 15:53:25 -0600

    Add #order_by_revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,7 +1,19 @@
 class Merchant < ApplicationRecord
   has_many :items
+  has_many :invoices
+  has_many :invoice_items, through: :items
 
-  def self.find_by_name(query)
-    where("name ilike ?", "%#{query}%").order(:name).first
+  class << self
+    def find_by_name(query)
+      where("name ilike ?", "%#{query}%").order(:name).first
+    end
+
+    def order_by_revenue(quantity)
+       joins(:invoice_items)
+      .select('merchants.*, sum(quantity * invoice_items.unit_price) as revenue')
+      .group(:id)
+      .order(revenue: :desc)
+      .limit(quantity)
+    end
   end
 end
