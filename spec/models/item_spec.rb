2ffa2ce7ac688009efd159dff682201784affc95
Henry Schmid <henry.schmid1@gmail.com>
2021-10-26 15:48:50 -0600

    Update test to have name instead of description

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   it { should belong_to(:merchant) }
+  it { should have_many(:invoice_items) }
+  it { should have_many(:invoices).through(:invoice_items) }
 
   it '#find_all_items' do
     merchant = create(:merchant)
     item1 = create(:item, name: 'ski bindings', merchant: merchant, unit_price: 10)
-    item2 = create(:item, description: 'Snowboard Bindings', merchant: merchant, unit_price: 20)
+    item2 = create(:item, name: 'Snowboard Bindings', merchant: merchant, unit_price: 20)
     item3 = create(:item, name: 'boots', merchant: merchant, unit_price: 30)
     item4 = create(:item, name: 'helmet', merchant: merchant, unit_price: 30)
     params = { name: 'bInD' }
