c1e1b148e345475cab392da8629a4988e8a11b0d
Henry Schmid <henry.schmid1@gmail.com>
2021-10-25 20:21:23 -0600

    Add test for finding merchants

---
+++ spec/requests/v1/merchants_find_request_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+describe 'find merchants API' do
+  before :each do
+    create_list(:merchant, 50)
+    create(:merchant, name: 'Icelantic')
+    create(:merchant, name: 'Land Rover')
+  end
+
+  it 'get first ordered merchant containing name param' do
+    get '/api/v1/merchants/find?name=LaN'
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant[:data]).to be_a Hash
+    expect(merchant[:data].length).to eq(3)
+    expect(merchant[:data][:attributes].length).to eq(1)
+    expect(merchant[:data][:attributes][:name]).to eq('Icelantic')
+  end
+
+  it 'params cant be missing or empty' do
+    get '/api/v1/merchants/find?name='
+
+    expect(response).to have_http_status(400)
+
+    get '/api/v1/merchants/find'
+
+    expect(response).to have_http_status(400)
+  end
+
+  it 'returns a successful response and error message for ' do
+    get '/api/v1/merchants/find?name=wrongname'
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    message =
+      {
+        data: {
+          message: "no merchant name found including 'wrongname'"
+        }
+      }
+
+    expect(json).to eq(message)
+  end
+end
