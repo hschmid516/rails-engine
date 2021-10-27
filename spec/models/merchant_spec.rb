10ba912ce03e440a205781ffd79331d1c5515d6c
Henry Schmid <henry.schmid1@gmail.com>
2021-10-26 21:12:37 -0600

    Add test for most items

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Merchant, type: :model do
     expect(Merchant.find_by_name('LaN')).to eq(merch1)
   end
 
-  it '#order_by_revenue' do
+  before :each do
     @merch1 = create(:merchant)
     item1 = create(:item, merchant: @merch1)
     cust1 = create(:customer)
@@ -22,23 +22,29 @@ RSpec.describe Merchant, type: :model do
     item2 = create(:item, merchant: @merch2)
     cust2 = create(:customer)
     inv2 = @merch2.invoices.create(customer: cust2, status: 'shipped')
-    inv2.invoice_items.create(item: item2, quantity: 10, unit_price: 200)
+    inv2.invoice_items.create(item: item2, quantity: 20, unit_price: 200)
     inv2.transactions.create(result: 'success')
 
     @merch3 = create(:merchant)
     item3 = create(:item, merchant: @merch3)
     cust3 = create(:customer)
     inv3 = @merch3.invoices.create(customer: cust3, status: 'shipped')
-    inv3.invoice_items.create(item: item3, quantity: 10, unit_price: 300)
+    inv3.invoice_items.create(item: item3, quantity: 30, unit_price: 300)
     inv3.transactions.create(result: 'success')
 
     @merch4 = create(:merchant)
     item4 = create(:item, merchant: @merch4)
     cust4 = create(:customer)
     inv4 = @merch4.invoices.create(customer: cust4, status: 'shipped')
-    inv4.invoice_items.create(item: item4, quantity: 10, unit_price: 1000)
+    inv4.invoice_items.create(item: item4, quantity: 40, unit_price: 1000)
     inv4.transactions.create(result: 'failed')
+  end
 
+  it '#order_by_revenue' do
     expect(Merchant.order_by_revenue(2)).to eq([@merch3, @merch2])
+  end
+
+  it '#most_items' do
+    expect(Merchant.most_items(2)).to eq([@merch3, @merch2])
   end
 end
