83e81dfae9aefb83fe61e873070c2bf088405723
Henry Schmid <henry.schmid1@gmail.com>
2021-10-27 16:26:15 -0600

    Add endpoint to find total revenue for date range

--- app/controllers/api/v1/revenue/merchants_controller.rb
+++ app/controllers/api/v1/revenue/merchants_controller.rb
@@ -10,4 +10,17 @@ class Api::V1::Revenue::MerchantsController < ApplicationController
       render json: MerchantNameRevenueSerializer.new(merchants)
     end
   end
+
+  def date_range
+    if (params[:start] && params[:end]) && !(params[:start].empty? || params[:end].empty?)
+      revenue = Merchant.revenue_for_range(params[:start], params[:end])[0]
+      render json: RevenueRangeSerializer.new(revenue)
+    else
+      render json: {
+        message: "revenue could not be found",
+        error: nil,
+        }, status: 400
+    end
+  end
 end
+# if params.has_key?(:start) && params.has_key?(:end)
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,18 +3,19 @@ class Merchant < ApplicationRecord
   has_many :invoices
   has_many :invoice_items, through: :items
 
+  scope :revenue, -> { joins(invoices: :invoice_items)
+                      .merge(Invoice.successful_invoices)
+                      .select('merchants.*, sum(quantity * invoice_items.unit_price) as revenue') }
+
   class << self
     def find_by_name(query)
       where("name ilike ?", "%#{query}%").order(:name).first
     end
 
     def order_by_revenue(quantity)
-       joins(invoices: :invoice_items)
-      .merge(Invoice.successful_invoices)
-      .select('merchants.*, sum(quantity * invoice_items.unit_price) as revenue')
-      .group(:id)
-      .order(revenue: :desc)
-      .limit(quantity)
+       revenue.group(:id)
+              .order(revenue: :desc)
+              .limit(quantity)
     end
 
     def most_items(quantity)
@@ -24,6 +25,13 @@ class Merchant < ApplicationRecord
       .group(:id)
       .order(item_count: :desc)
       .limit(quantity)
+    end
+
+    def revenue_for_range(start_date, end_date)
+       joins(invoices: :invoice_items)
+      .merge(Invoice.successful_invoices)
+      .select('sum(quantity * invoice_items.unit_price) as revenue')
+      .where(invoices: { created_at: start_date...end_date.to_date.end_of_day })
     end
   end
 end
