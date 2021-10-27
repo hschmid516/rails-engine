b9cdb07d33c46b83baa4c69730435dfe6998c4ed
Henry Schmid <henry.schmid1@gmail.com>
2021-10-26 18:58:03 -0600

    Add error handling to index action

--- app/controllers/api/v1/revenue/merchants_controller.rb
+++ app/controllers/api/v1/revenue/merchants_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::Revenue::MerchantsController < ApplicationController
   def index
-    merchants = Merchant.order_by_revenue(params[:quantity])
-    render json: MerchantRevenueSerializer.new(merchants)
+    if !params[:quantity] || params[:quantity].to_i < 1
+      render json: {
+        message: "merchants could not be found",
+        error: "query params must be present and not empty",
+        }, status: 400
+    else
+      merchants = Merchant.order_by_revenue(params[:quantity])
+      render json: MerchantNameRevenueSerializer.new(merchants)
+    end
   end
 end
