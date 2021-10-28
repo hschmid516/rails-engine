9893a49aaac1f1f5cb9cdc2192897616cd8a9b15
Henry Schmid <henry.schmid1@gmail.com>
2021-10-27 18:55:34 -0600

    Refactor error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   def no_object_error(id)
     render json: {
         message: "your query could not be completed",
-        errors: ["no object found with id: #{id}"],
+        error: ["no object found with id: #{id}"],
       }, status: 404
   end
 
